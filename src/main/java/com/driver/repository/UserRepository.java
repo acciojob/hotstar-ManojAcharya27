becb118f7da7f95226ecf73e3bc7bb9f7b0f9301
unknown <hmanojacharya@gmail.com>
2023-05-05 14:18:37 +0530

    Hotstar 1.1

--- src/main/java/com/driver/repository/UserRepository.java
+++ src/main/java/com/driver/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.driver.repository;
 
 import com.driver.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
 }
